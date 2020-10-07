94657eee206abcac82e701f7d26a373a60381b54
vitormosso <vitormosso2@gmail.com>
2020-10-07 16:45:25 -0300

    added css to app component, link to detail page from dashboard and heroes list after clicking a hero, added a button to go back from the detail page, added a method on heroes service to fetch only one hero based on its id given by the route

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,11 +11,9 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[ ];
-  selectedHero: Hero;
 
   constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
+    private heroService: HeroService
   ) { }
 
   ngOnInit(): void {
@@ -24,11 +22,6 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
 }
